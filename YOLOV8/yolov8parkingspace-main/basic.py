c0e3139f5d47c620068b3995781248438ed53e15
Renato Wilker <renatowilkerpaulasilva9@gmail.com>
2024-04-25 16:48:04 -0300

    Melhorias no Frontend e no Backend

--- YOLOV8/yolov8parkingspace-main/basic.py
+++ YOLOV8/yolov8parkingspace-main/basic.py
@@ -6,12 +6,12 @@ import json
 import time
 import threading
 
-dados_yolo = {"carro": 0}
+yolo_data = {"person": 0, "bicycle": 0, "motorcycle": 0, "car": 0, "truck": 0}
 
 # CONSTANTES
-DADOS_YOLO_PATH = 'D:\\Documents\\ProjetoIntegradorV\\frontend\\dados_yolo.json'
-VIDEO_PATH = 'D:/Documents/ProjetoIntegradorV/YOLOV8/yolov8parkingspace-main/parking1.mp4'
-CLASSES_PATH = 'D:/Documents/ProjetoIntegradorV/YOLOV8/yolov8parkingspace-main/classes.txt'
+DADOS_YOLO_PATH = '../ProjetoIntegradorV/frontend/dados_yolo.json'
+VIDEO_PATH = '../ProjetoIntegradorV/YOLOV8/yolov8parkingspace-main/parking1.mp4'
+CLASSES_PATH = '../ProjetoIntegradorV/YOLOV8/yolov8parkingspace-main/classes.txt'
 AREA_9 = [(500,327),(550,388),(610,380),(555,324)]
 
 # INICIALIZAÇÃO YOLO
@@ -23,15 +23,49 @@ def mouse_event_handler(event, x, y, flags, param):
         colorsBGR = [x, y]
         print(colorsBGR)
 
-def escrever_dados_json():
+# PROCESSAR FRAME DO VÍDEO
+def process_frame(frame, model, class_list):
+    frame = cv2.resize(frame, (1020, 500))
+    results = model.predict(frame)
+    px = pd.DataFrame(results[0].boxes.xyxy).astype("float")
+    list9 = []
+
+    for index, row in px.iterrows():
+        x1, y1, x2, y2 = map(int, row[:4])
+        d = int(row.name)
+        c = class_list[d]
+
+        if 'person' in c or 'bicycle' in c or 'motorcycle' in c or 'car' in c or 'truck' in c:
+            centerX = (x1 + x2) // 2
+            centerY = (y1 + y2) // 2
+            results9 = cv2.pointPolygonTest(np.array(AREA_9, np.int32), ((centerX, centerY)), False)
+            if results9 >= 0:
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.circle(frame, (centerX, centerY), 3, (0, 0, 255), -1)
+                list9.append(c)
+            yolo_data[c] = len(list9)
+
+    return frame, len(list9)
+
+# DESENHAR ÁREA NO VÍDEO
+def draw_area(frame, count):
+    if count == 1:
+        cv2.polylines(frame, [np.array(AREA_9,np.int32)], True, (0,0,255), 2)
+        cv2.putText(frame, str('9'), (591,398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1)
+    else:
+        cv2.polylines(frame, [np.array(AREA_9,np.int32)], True, (0,255,0), 2)
+        cv2.putText(frame, str('9'), (591,398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,255,255), 1)
+
+# ESCREVER OS DADOS NO JSON
+def write_data_json():
     while True:
         with open(DADOS_YOLO_PATH, 'w') as f:
-            json.dump(dados_yolo, f)
+            json.dump(yolo_data, f)
         time.sleep(1)
 
 # FUNÇÃO PRINCIPAL
 def main():
-    global dados_yolo
+    global yolo_data
 
     cv2.namedWindow('RGB')
     cv2.setMouseCallback('RGB', mouse_event_handler)
@@ -42,60 +76,21 @@ def main():
     data = classes_file.read()
     class_list = data.split("\n")
 
-    while True:    
+    while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        frame = cv2.resize(frame, (1020,500))
-
-        results = model.predict(frame)
-        #print(results)
-        a = results[0].boxes.xyxy
-        #print(a)
-        px = pd.DataFrame(a).astype("float")
-        #print(px)
-
-        list9 = []
-    
-        for index, row in px.iterrows():
-            print(f"\nRow: {index}")
-            print(row)
-    
-            x1 = int(row[0])
-            y1 = int(row[1])
-            x2 = int(row[2])
-            y2 = int(row[3])
-            d = int(row.name)
-            c = class_list[d]
-
-            if 'car' in c:
-                cx = int(x1+x2) // 2
-                cy = int(y1+y2) // 2
-
-                results9 = cv2.pointPolygonTest(np.array(AREA_9, np.int32), ((cx,cy)), False)
-                if results9 >= 0:
-                    cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
-                    cv2.circle(frame, (cx,cy), 3, (0,0,255), -1)
-                    list9.append(c)
-            
-        a9 = (len(list9))
-        dados_yolo["carro"] = a9
-
-        if a9 == 1:
-            cv2.polylines(frame, [np.array(AREA_9,np.int32)], True, (0,0,255), 2)
-            cv2.putText(frame, str('9'), (591,398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1)
-        else:
-            cv2.polylines(frame, [np.array(AREA_9,np.int32)], True, (0,255,0), 2)
-            cv2.putText(frame, str('9'), (591,398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,255,255), 1)
+        frame, a9 = process_frame(frame, model, class_list)
+        draw_area(frame, a9)
 
         cv2.imshow("RGB", frame)
 
-        if cv2.waitKey(1)&0xFF == 27:
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 
-    thread_escrita_dados = threading.Thread(target=escrever_dados_json)
-    thread_escrita_dados.start()
+    thread_write_data = threading.Thread(target=write_data_json)
+    thread_write_data.start()
     cap.release()
     cv2.destroyAllWindows()
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,12 @@
 function carregarDadosYOLO() {
-    fetch('D:\\Documents\\ProjetoIntegradorV\\frontend\\dados_yolo.json')
+    fetch('../frontend/dados_yolo.json')
         .then(response => response.json())
         .then(data => {
-            document.getElementById("carro").innerText = data.carro.toString();
+            document.getElementById("bicycle").innerText = data.bicycle.toString();
+            document.getElementById("motorcycle").innerText = data.motorcycle.toString();
+            document.getElementById("car").innerText = data.car.toString();
+            document.getElementById("truck").innerText = data.truck.toString();
+            document.getElementById("person").innerText = data.person.toString();
         })
         .catch(error => console.error('Erro ao carregar os dados do YOLO:', error));
 }
